e2027c9990b9e13941d11fe41a119353c52e3594
Amanda Nelson <amandaln4311@gmail.com>
2021-01-21 18:48:34 -0500

    fixed js files, added validator

--- app.js
+++ app.js
@@ -4,15 +4,223 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const validator = require('email-validator');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { empty } = require("rxjs");
 
-
+let teamMembers = [];
+let employeeID = 1;
+let newEmployee;
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function appMenu(){
+    createManager();
+    
+    function createManager() {
+        console.log("Please build your team");
+        inquirer.prompt([
+            {
+                type:"input",
+                name:"managerName",
+                message:"What is your manager’s name?"
+                //Validate user input
+            },
+            {
+                type:"input",
+                name:"managerId",
+                message:"What is your manager’s id?"
+                //Validate user input
+            },
+            {
+                type:"input",
+                name:"managerEmail",
+                message: "What is your manager’s email address?",
+                validate: function(email)
+                {
+                    valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                    if (valid) {
+                        return true;
+                    }
+                    else{
+                        console.log("\nEnter valid email");
+                        
+                        return false;
+                    }
+                }
+                
+            },
+            {
+                type:"input",
+                name:"managerOfficeNum",
+                message: "What is your manager’s office number?"
+                //Validate user input
+            },
+            {
+                type:"list",
+                name:"moreEmployees",
+                message: "Do you want to add more employees?",
+                choices: ["Intern", "Engineer", "No"]
+                //Validate user input
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNum);
+            console.log(manager);
+            teamMembers.push(manager)
+            if (answers.moreEmployees === "Intern") {
+                createIntern();
+            }
+            else if (answers.moreEmployees === "Engineer") {
+                createEngineer();
+            }
+            else {
+                buildHTML();
+            }
+
+        });
+    }
+    function createEngineer() {
+        console.log("Please build your team");
+        inquirer.prompt([
+            {
+                type:"input",
+                name:"engineerName",
+                message:"What is your engineer’s name?"
+                //Validate user input
+            },
+            {
+                type:"input",
+                name:"engineerId",
+                message:"What is your engineer’s id"
+                //Validate user input
+            },
+            {
+                type:"input",
+                name:"engineerEmail",
+                message:"What is your engineer’s email address?",
+                //Validate user input
+                validate: function(email)
+                {
+                    valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                    if (valid) {
+                        return true;
+                    }
+                    else{
+                        console.log("\nEnter valid email");
+                        
+                        return false;
+                    }
+                }
+            },
+            {
+                type:"input",
+                name:"engineerGithub",
+                message:"What is your engineer’s github account?"
+                //Validate user input
+            },
+            {
+                type:"list",
+                name:"moreEmployees",
+                message: "Do you want to add more employees?",
+                choices: ["Intern", "Engineer", "No"],
+                //Validate user input
+            },
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            console.log(engineer);
+            teamMembers.push(engineer)
+            if (answers.moreEmployees === "Intern") {
+                createIntern();
+            }
+            else if (answers.moreEmployees === "Engineer") {
+                createEngineer();
+            }
+            else {
+                buildHTML();
+            }
+        });
+    }
+    function createIntern() {
+        console.log("Please build your team");
+        inquirer.prompt([
+            {
+                type:"input",
+                name:"internsName",
+                message:"What is your intern’s name?"
+                
+            },
+            {
+                type:"input",
+                name:"internId",
+                message:"What is your intern’s id"
+                //Validate user input
+            },
+            {
+                type:"input",
+                name:"internEmail",
+                message:"What is your intern’s email address?",
+                // Validate email
+                validate: function(email)
+                {
+                    valid = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+                    if (valid) {
+                        return true;
+                    }
+                    else{
+                        console.log("\nEnter valid email");
+                        
+                        return false;
+                    }
+                }
+            },
+            {
+                type:"input",
+                name:"internSchool",
+                message:"What is your intern's school?"
+               
+            },
+            {
+                type:"list",
+                name:"moreEmployees",
+                message: "Do you want to add more employees?",
+                choices: ["Intern", "Engineer", "No"],
+                
+            },
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            console.log(intern);
+            teamMembers.push(intern);
+            if (answers.moreEmployees === "Intern") {
+                createIntern();
+            }
+            else if (answers.moreEmployees === "Engineer") {
+                createEngineer();
+            }
+            else {
+                buildHTML();
+            }
+        });
+    }
+
+    function buildHTML() {
+        console.log("buildinghtml")
+        const newPage = render(teamMembers);
+//    actually write the file
+    fs.writeFile("./teamPage.html", newPage, (err) =>
+    err ? console.log(err) : console.log('Success!')
+);
+    }
+
+   
+    
+}
+
+
+appMenu();
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
